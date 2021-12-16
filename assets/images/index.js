413fce14cadb3837cb5a75505d4a3f6a87533a27
Ramil <vk2011@bk.ru>
2021-12-16 12:24:18 +0500

    added Grid MUI

--- assets/images/index.js
+++ assets/images/index.js
@@ -1,5 +1,21 @@
 import MainLogo from './icons/main_logo.png';
+import Binance from './icons/binance.svg';
+import BinanceLeft from './icons/binance_left.svg';
+import BinanceRight from './icons/binance_right.svg';
+import Bitmex from './icons/bitmex.svg';
+import Bybit from './icons/bybit.svg';
+import Ftx from './icons/ftx.svg';
+import Hitbtc from './icons/hitbtc.svg';
+import Okex from './icons/okex.svg';
 
 export {
     MainLogo,
+    Binance,
+    BinanceLeft,
+    BinanceRight,
+    Bitmex,
+    Bybit,
+    Ftx,
+    Hitbtc,
+    Okex
 }
--- components/appMain/AppMain.js
+++ components/appMain/AppMain.js
@@ -1,8 +1,19 @@
+import Image from "next/image";
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { styled } from "@mui/system";
+import { Grid } from "@mui/material";
 
 import AppButton from "../appButton/AppButton";
+
+import { Binance } from "../../assets/images";
+import { Bybit } from "../../assets/images";
+import { Okex } from "../../assets/images";
+import { Ftx } from "../../assets/images";
+import { Bitmex } from "../../assets/images";
+import { Hitbtc } from "../../assets/images";
+import { BinanceLeft } from "../../assets/images";
+import { BinanceRight } from "../../assets/images";
 
 const OrderList = styled('ol')({
     display: 'flex',
@@ -16,7 +27,8 @@ const ListItem = styled('li')({
     height: '12px',
     border: '1px solid #C49BFF',
     boxShadow: '0px 0px 20px rgba(188, 79, 255, 0.7)',
-    transform: 'rotate(45deg)'
+    transform: 'rotate(45deg)',
+    cursor: 'pointer'
 })
 
 const AppMain = () => {
@@ -24,7 +36,7 @@ const AppMain = () => {
         <main>
 
             <Box sx={{
-                width: '680px'
+                maxWidth: '650px'
             }}>
                 <Typography variant="h1" sx={{
                     fontSize: '50px',
@@ -61,6 +73,35 @@ const AppMain = () => {
                 <ListItem/>
                 <ListItem/>
             </OrderList>
+
+            <Grid columnSpacing={6} container alignItems="center" justifyContent="center" sx={{
+                marginTop: '50px'
+            }}>
+                <Grid rowSpacing={2} container item xs={2} justifyContent="center">
+                    <Image src={Binance}/>
+                    <Grid item xs={6}>
+                        <Image src={BinanceLeft}/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Image src={BinanceRight}/>
+                    </Grid>
+                </Grid>
+                <Grid item xs={2}>
+                    <Image src={Bybit}/>
+                </Grid>
+                <Grid item xs={2}>
+                    <Image src={Okex}/>
+                </Grid>
+                <Grid item xs={2}>
+                    <Image src={Ftx}/>
+                </Grid>
+                <Grid item xs={2}>
+                    <Image src={Bitmex}/>
+                </Grid>
+                <Grid item xs={2}>
+                    <Image src={Hitbtc}/>
+                </Grid>
+            </Grid>
         </main>
     )
 }
